e5992eb1552f84b161aaa21167eaf953547b32f6
guido morabito <guidomorabito161@hotmail.com>
2025-01-28 08:45:24 -0300

    seed finished

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 yarn add @nestjs/config
 
 docker-compose up -d# nest-typeorm
+
+http://localhost:3000/api/seed
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
+import { Product } from 'src/products/entities/product.entity';
 
 
 @Injectable()
@@ -16,6 +18,16 @@ export class SeedService {
 
   private async insertNewProducts(){
     await this.productsService.deleteAllProducts()
+
+    const products = initialData.products
+    const insertPromises:any = []
+
+    products.forEach(product => {
+      insertPromises.push(this.productsService.create(product))
+    })
+
+    await Promise.all(insertPromises)
+
     return true
   }
 }
