46fd399a04b44eaa680ccab17eb8a49b65ded994
guido <gmorabito@makingsense.com>
2025-02-04 11:51:44 -0300

    register and login

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -29,7 +30,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     ProductsModule,
     CommonModule,
     SeedModule,
-    FilesModule
+    FilesModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
